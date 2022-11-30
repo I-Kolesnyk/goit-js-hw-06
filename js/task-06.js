a4f86777d0cab67db617dd45dc252b206d76450d
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2022-11-30 04:42:49 +0200

    updates task 6 solution

--- js/task-06.js
+++ js/task-06.js
@@ -1,24 +1,16 @@
-const inputRef = document.querySelector('.validation-input');
+const inputRef = document.querySelector('#validation-input');
 
 inputRef.addEventListener('blur', handleInputValidation);
 inputRef.addEventListener('focus', handleInputFocus);
 
 function handleInputValidation(event) {
-  if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
-    inputRef.classList.add('valid');
-    console.log(inputRef);
-  } else {
-    inputRef.classList.add('invalid');
-    console.log(inputRef);
-  }
+  event.currentTarget.value.length === Number(inputRef.dataset.length)
+    ? inputRef.classList.add('valid')
+    : inputRef.classList.add('invalid');
 }
 
 function handleInputFocus(event) {
-  if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
-    inputRef.classList.remove('valid');
-    console.log(inputRef);
-  } else {
-    inputRef.classList.remove('invalid');
-    console.log(inputRef);
-  }
+  event.currentTarget.value.length === Number(inputRef.dataset.length)
+    ? inputRef.classList.remove('valid')
+    : inputRef.classList.remove('invalid');
 }
